c97d93a39fc5205ab1b1958b4cd41612cd2be598
Alok Pahan <alokpahan18@gmail.com>
2021-09-24 12:55:21 +0530

    implementation of multiplication

--- 2nd.c
+++ 2nd.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 int main()
 {
-    int a =5;
-    int b= 6;
-    printf("Sum of 'a' and 'b' is %d ", a+b); 
+    int a = 5;
+    int b = 6;
+    printf("Sum of 'a' and 'b' is %d ", a + b);
+    printf("multiplication of 'a' and 'b' is %d ", a * b);
 
     return 0;
 }
