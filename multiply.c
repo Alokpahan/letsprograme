423147d48d35e5a6a5cf7d8691ca585bd74b1251
Alok Pahan <alokpahan18@gmail.com>
2021-09-24 13:16:13 +0530

    changes

--- multiply.c
+++ multiply.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 int main()
 {
-    int a=12,b=10;
-    printf("The product of 'a' and 'b' is  %d", a*b);
-
+    int a = 12, b = 10;
+    printf("The product of 'a' and 'b' is  %d", a * b);
+    printf("The product of 'a' and 'b' is  %d", a * b);
+    printf("The product of 'a' and 'b' is  %d", a * b);
     return 0;
 }
